c1d6b5f8e5424a39a76123d9f659a710a97436f7
wuxh <wxh1220@gmail.com>
2023-02-03 21:27:26 +0800

    feat(common): add `tinyCloneDeep`

---
+++ src/Common/tinyCloneDeep.ts
@@ -0,0 +1,34 @@
+/**
+ * 克隆一个值（仅支持对象和数组
+ * @param 待克隆的值
+ * @returns 克隆后的值
+ * @example
+ * const obj = { a: 1, b: { c: 2 }, d: [3, 4] };
+ * const obj2 = tinyCloneDeep(obj);
+ */
+function tinyCloneDeep(val: any) {
+  if (Array.isArray(val))
+    return cloneArrayDeep(val)
+  else if (typeof val === 'object' && val !== null)
+    return cloneObjectDeep(val)
+
+  return val
+}
+
+function cloneObjectDeep(val: any) {
+  if (Object.getPrototypeOf(val) === Object.prototype) {
+    const res: Record<string, any> = {}
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in val)
+      res[key] = tinyCloneDeep(val[key])
+
+    return res
+  }
+  return val
+}
+
+function cloneArrayDeep(val: any[]) {
+  return val.map<any>(item => tinyCloneDeep(item))
+}
+
+export default tinyCloneDeep
