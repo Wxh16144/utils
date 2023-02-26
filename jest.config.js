aa70e5426d969a55d8d7492494490a042b890538
wuxh <wxh1220@gmail.com>
2023-02-27 06:17:51 +0800

    chore: jest Ignore ts type errors

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,11 @@ module.exports = {
   testURL: 'https://jestjs.io/',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        exclude: ['**'],
+      },
+    },
+  },
 }
