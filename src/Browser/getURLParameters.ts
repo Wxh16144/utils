c00f4202ab634a9d787f03375a6965db7cd6084e
wuxh <wxh1220@gmail.com>
2023-02-27 08:25:04 +0800

    chore: fix typod

--- src/Browser/getURLParameters.ts
+++ src/Browser/getURLParameters.ts
@@ -1,3 +1,6 @@
+
+type Obj = Record<string, any>
+
 /**
  * 创建一个包含URL参数的对象
  * @param url 字符串 `default: window?.location?.search`
@@ -8,8 +11,8 @@
  * getURLParameters('http://url.com/page?name=Adam&surname=Smith');
  * // {name: 'Adam', surname: 'Smith'}
  */
-const getURLParameters = (url = window?.location?.search): Record<string, any> =>
-  (url.match(/([^?=&]+)(=([^&]*))/g) || [])
+const getURLParameters = (url = window?.location?.search): Obj =>
+  ((url.match(/([^?=&]+)(=([^&]*))/g) || []) as Obj[])
     .reduce<Record<string, string>>(
     (a, v) => {
       (a[v.slice(0, v.indexOf('='))] = v.slice(v.indexOf('=') + 1))
