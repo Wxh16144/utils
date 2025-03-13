0b1845244d9d31c71b3a9ccfb5032f41bc2885b3
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-03-13 15:23:47 +0800

    feat: add `toArray` `isNil` (#9)

--- src/Common/toArray.ts
+++ src/Common/toArray.ts
@@ -1,10 +1,13 @@
-/**
- * Converts a value to an array.
- * @param value any value
- * @returns always an array
- */
-function toArray<T>(value: T | T[]): T[] {
-  return Array.isArray(value) ? value : [value]
+import isNil from './isNil'
+
+const toArray = <T>(value: T | T[]): NonNullable<T>[] => {
+  if (isNil(value)) {
+    return []
+  }
+
+  return Array.isArray(value)
+    ? value as NonNullable<T>[]
+    : [value] as NonNullable<T>[]
 }
 
 export default toArray
