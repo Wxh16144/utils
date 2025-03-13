d60aa123acf0b0d5ae9a5439b30995abcbea2161
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-03-13 14:59:32 +0800

    feat: add `toArray` `isNil`

---
+++ src/Common/isNil.ts
@@ -0,0 +1,12 @@
+/**
+ * 判断一个值是否为 null 或 undefined
+ * @param value 要判断的值
+ * @returns 是则返回 true，否则返回 false
+ * @example
+ * isNil(null) // => true
+ * isNil(undefined) // => true
+ * isNil(0) // => false
+ */
+const isNil = (value: any): boolean => value === null || value === undefined
+
+export default isNil
